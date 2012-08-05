dbe986856b93c021c92cff11b7530e52b1abea07
Joona Lugasi <joona.lugasi@hotmail.fi>
2012-08-06 02:26:58 +0300

    vaihdoin aihetta kekovertailuun

---
+++ Kekovertailu/src/Logiikka/Binaarikeko.java
@@ -0,0 +1,15 @@
+
+package Logiikka;
+
+public class Binaarikeko {
+    private int[] taulukko;
+    private int koko;
+    
+    public Binaarikeko()    {
+        taulukko = new int[10];
+        koko = 0;
+    }
+    
+    
+    
+}
--- Kekovertailu/src/Logiikka/Main.java
+++ Kekovertailu/src/Logiikka/Main.java
@@ -1,12 +1,8 @@
 
 package Logiikka;
 
-/**
- *
- * @author lugasi
- */
 public class Main {
-
+    
     public static void main(String[] args) {
         
     }
